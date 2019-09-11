3a628231651e73a9d83fb9930d7bff4b56cd29bb
Raúl González <rafex.dev@gmail.com>
2019-09-11 17:06:49 -0500

    correccion de R a r

--- rafex/myapp/src/main/java/mx/rafex/utilidades/CalculadoraImplementacion.java
+++ rafex/myapp/src/main/java/mx/rafex/utilidades/CalculadoraImplementacion.java
@@ -2,40 +2,40 @@ package mx.rafex.utilidades;
 
 public class CalculadoraImplementacion implements Calculadora {
 
-	public int suma(final int valorUno, final int valorDos) {
-		int Resultado = valorUno + valorDos;
-		return 0;
-	}
+    public int suma(final int valorUno, final int valorDos) {
+        final int resultado = valorUno + valorDos;
+        return resultado;
+    }
 
-	public int resta(final int valorUno, final int valorDos) {
-		int Resultado = valorUno - valorDos;
-		return Resultado;
-	}
+    public int resta(final int valorUno, final int valorDos) {
+        final int resultado = valorUno - valorDos;
+        return resultado;
+    }
 
-	public int multiplicacion(final int valorUno, final int valorDos) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    public int multiplicacion(final int valorUno, final int valorDos) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
 
-	public int division(final int valorUno, final int valorDos) {
-		int Resultado = 0;
-		Resultado = valorUno / valorDos;
-		return Resultado;
-	}
+    public int division(final int valorUno, final int valorDos) {
+        int resultado = 0;
+        resultado = valorUno / valorDos;
+        return resultado;
+    }
 
-	public int raizCuadrada(final int numero) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    public int raizCuadrada(final int numero) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
 
-	public int potencia(final int numero, final int exponente) {
-		int result;
-		result = (int) Math.pow(numero, exponente);
-		return result;
-	}
+    public int potencia(final int numero, final int exponente) {
+        int result;
+        result = (int) Math.pow(numero, exponente);
+        return result;
+    }
 
-	public void enviarSaludo(final String nombre) {
-		System.out.println("Un saludos para: " + nombre);
-	}
+    public void enviarSaludo(final String nombre) {
+        System.out.println("Un saludos para: " + nombre);
+    }
 
 }

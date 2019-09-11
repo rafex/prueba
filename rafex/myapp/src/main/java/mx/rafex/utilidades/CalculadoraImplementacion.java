c1dd21e7f1403afb0a8752c04ff3851b3d65c809
Eduardo_Villafranco <eduagr.v@gmail.com>
2019-09-11 17:12:07 -0500

    Potencia resultado

--- rafex/myapp/src/main/java/mx/rafex/utilidades/CalculadoraImplementacion.java
+++ rafex/myapp/src/main/java/mx/rafex/utilidades/CalculadoraImplementacion.java
@@ -2,41 +2,41 @@ package mx.rafex.utilidades;
 
 public class CalculadoraImplementacion implements Calculadora {
 
-    public int suma(final int valorUno, final int valorDos) {
-        final int resultado = valorUno + valorDos;
-        return resultado;
-    }
+	public int suma(final int valorUno, final int valorDos) {
+		final int resultado = valorUno + valorDos;
+		return resultado;
+	}
 
-    public int resta(final int valorUno, final int valorDos) {
-        final int resultado = valorUno - valorDos;
-        return resultado;
-    }
+	public int resta(final int valorUno, final int valorDos) {
+		final int resultado = valorUno - valorDos;
+		return resultado;
+	}
 
-    public int division(final int valorUno, final int valorDos) {
-        int resultado = 0;
-        resultado = valorUno / valorDos;
-        return resultado;
-    }
+	public int division(final int valorUno, final int valorDos) {
+		int resultado = 0;
+		resultado = valorUno / valorDos;
+		return resultado;
+	}
 
-    public int potencia(final int numero, final int exponente) {
-        int result;
-        result = (int) Math.pow(numero, exponente);
-        return result;
-    }
+	public int potencia(final int numero, final int exponente) {
+		int resultado;
+		resultado = (int) Math.pow(numero, exponente);
+		return resultado;
+	}
 
-    public int multiplicacion(final int valorUno, final int valorDos) {
-        final int resultado = valorUno * valorDos;
-        return resultado;
-    }
+	public int multiplicacion(final int valorUno, final int valorDos) {
+		final int resultado = valorUno * valorDos;
+		return resultado;
+	}
 
-    public int raizCuadrada(final int numero) {
+	public int raizCuadrada(final int numero) {
 
-        final int resultado = (int) Math.sqrt(numero);
-        return resultado;
-    }
+		final int resultado = (int) Math.sqrt(numero);
+		return resultado;
+	}
 
-    public void enviarSaludo(final String nombre) {
-        System.out.println("Un saludos para: " + nombre);
-    }
+	public void enviarSaludo(final String nombre) {
+		System.out.println("Un saludos para: " + nombre);
+	}
 
 }

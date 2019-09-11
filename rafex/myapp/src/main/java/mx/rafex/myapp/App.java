9ce97220cd9b7a7c35aa4c7e6bd8b580ec1fe17d
Raúl González <rafex.dev@gmail.com>
2019-09-11 17:26:48 -0500

    empaquetando cdddigo

--- rafex/myapp/src/main/java/mx/rafex/myapp/App.java
+++ rafex/myapp/src/main/java/mx/rafex/myapp/App.java
@@ -13,7 +13,34 @@ public class App {
 
         final Calculadora objetoCalculadora = new CalculadoraImplementacion();
 
-        objetoCalculadora.enviarSaludo("Pedro!!");
+        int numeroUno, numeroDos;
+
+        numeroUno = Integer.parseInt(args[0]);
+        numeroDos = Integer.parseInt(args[1]);
+
+        final String nombre = args[2];
+
+        int resultado = objetoCalculadora.suma(numeroUno, numeroDos);
+
+        System.out.println("El resultado de la suma es: " + resultado);
+
+        resultado = objetoCalculadora.division(numeroUno, numeroDos);
+
+        System.out.println("El resultado de la division es: " + resultado);
+
+        resultado = objetoCalculadora.potencia(numeroUno, numeroDos);
+
+        System.out.println("El resultado de la potencia es: " + resultado);
+
+        resultado = objetoCalculadora.resta(numeroUno, numeroDos);
+
+        System.out.println("El resultado de la resta es: " + resultado);
+
+        resultado = objetoCalculadora.raizCuadrada(numeroUno);
+
+        System.out.println("El resultado de la raiz cuadrada es: " + resultado);
+
+        objetoCalculadora.enviarSaludo(nombre);
 
     }
 }

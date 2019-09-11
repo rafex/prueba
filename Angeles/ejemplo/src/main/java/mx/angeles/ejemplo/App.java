54cd79d3e8be1af8155870042145c8034788e012
Angeles <mariarojasonofre@gmail.com>
2019-09-11 13:32:32 -0500

    Proyecto

---
+++ Angeles/ejemplo/src/main/java/mx/angeles/ejemplo/App.java
@@ -0,0 +1,13 @@
+package mx.angeles.ejemplo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

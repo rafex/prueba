fdfe3e4403402d2fd0ed4bcf8322f6bf81ec20ff
degodrian <degodrian@gmail.com>
2019-09-11 16:35:09 -0500

    raiz;

--- rafex/myapp/src/main/java/mx/rafex/utilidades/CalculadoraImplementacion.java
+++ rafex/myapp/src/main/java/mx/rafex/utilidades/CalculadoraImplementacion.java
@@ -2,38 +2,41 @@ package mx.rafex.utilidades;
 
 public class CalculadoraImplementacion implements Calculadora {
 
-    public int suma(final int valorUno, final int valorDos) {
-        // TODO Auto-generated method stub
-        return 0;
-    }
+	public int suma(final int valorUno, final int valorDos) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
-    public int resta(final int valorUno, final int valorDos) {
-        // TODO Auto-generated method stub
-        return 0;
-    }
+	public int resta(final int valorUno, final int valorDos) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
-    public int multiplicacion(final int valorUno, final int valorDos) {
-        // TODO Auto-generated method stub
-        return 0;
-    }
+	public int multiplicacion(final int valorUno, final int valorDos) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
-    public int division(final int valorUno, final int valorDos) {
-        // TODO Auto-generated method stub
-        return 0;
-    }
+	public int division(final int valorUno, final int valorDos) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
-    public int raizCuadrada(final int numero) {
-        // TODO Auto-generated method stub
-        return 0;
-    }
+	public int raizCuadrada(final int numero) {
 
-    public int potencia(final int numero, final int exponente) {
-        // TODO Auto-generated method stub
-        return 0;
-    }
+		float resultado = (float) Math.sqrt(9);
+		System.out.println("La raiz cuadrada es " + resultado);
 
-    public void enviarSaludo(final String nombre) {
-        System.out.println("Un saludos para: " + nombre);
-    }
+		return numero;
+	}
+
+	public int potencia(final int numero, final int exponente) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public void enviarSaludo(final String nombre) {
+		System.out.println("Un saludos para: " + nombre);
+	}
 
 }
